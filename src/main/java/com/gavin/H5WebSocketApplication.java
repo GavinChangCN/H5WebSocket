eb57d5dff15653785704535b44718da477cf9bc0
章华隽 <gavinchangcn@163.com>
2017-03-07 09:33:31 +0800

    First commit

---
+++ src/main/java/com/gavin/H5WebSocketApplication.java
@@ -0,0 +1,12 @@
+package com.gavin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H5WebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H5WebSocketApplication.class, args);
+	}
+}
